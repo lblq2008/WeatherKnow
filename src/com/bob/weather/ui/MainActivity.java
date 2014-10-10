84d0a44afdd5fe6c19abfe526d9e2f829d78f733
Bob <lblq2011@163.com>
2014-10-10 21:44:55 +0800

    1.create project WeatherKnow

---
+++ src/com/bob/weather/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bob.weather.ui;
+
+import com.bob.weather.ui.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	
+}
