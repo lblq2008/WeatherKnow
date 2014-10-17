574ff532c22007fe32b556d69ae6b141470b6b85
Bob <lblq2011@163.com>
2014-10-17 15:43:51 +0800

    Chose city

--- src/com/bob/weather/ui/ChoseCityActivity.java
+++ src/com/bob/weather/ui/ChoseCityActivity.java
@@ -1,41 +1,52 @@
 package com.bob.weather.ui;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.bob.weather.config.Configs;
 import com.bob.weather.db.DBTableService;
 import com.bob.weather.inf.HttpCallBackListener;
 import com.bob.weather.model.City;
+import com.bob.weather.model.Country;
 import com.bob.weather.model.Province;
 import com.bob.weather.utils.HttpUtils;
 import com.bob.weather.utils.LogUtil;
 import com.bob.weather.utils.Utility;
 
-import android.R.color;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
+/**
+ * 城市选择页面
+ * @ClassName ChoseCityActivity
+ * @author Bob
+ * @date 2014-10-17 下午03:24:02 
+ */
 public class ChoseCityActivity extends BaseActivity {
+	
 	private List<Province> provinces;
 	private List<City> cities;
+	private List<Country> countries;
 	private DBTableService dbService = null;
-	private ListView lv_province, lv_city;
+	private ListView lv_province, lv_city, lv_country;
 	private ProvinceAdapter provincesAdapter;
-	private ArrayAdapter<City> citiesAdapter;
+	private CityAdapter citiesAdapter;
+	private CountryAdapter countriesAdapter;
 	private ProgressDialog progressDialog;
+	private TextView tv_province ,tv_city,tv_country ;
 
 	private Handler handler = new Handler() {
 		@Override
@@ -45,35 +56,39 @@ public class ChoseCityActivity extends BaseActivity {
 			switch (msg.what) {
 			case 0:
 				progressDialog.dismiss();
-				Toast.makeText(ChoseCityActivity.this, "加载数据异常",
-						Toast.LENGTH_SHORT).show();
-				// finish();
+				Toast.makeText(ChoseCityActivity.this, "加载数据异常",Toast.LENGTH_SHORT).show();
+				finish();
 				break;
 			case 1:
 				progressDialog.dismiss();
+				provinces = dbService.getAllProvinces();
 				provincesAdapter.refresh(provinces);
 				if (provinces.size() > 0) {
 					provincesAdapter.setSelectItem(0);
-					provincesAdapter.notifyDataSetInvalidated();
 					loadCity(provinces.get(0));
 				} else {
-					Toast.makeText(ChoseCityActivity.this, "无省份信息",
-							Toast.LENGTH_SHORT).show();
+					Toast.makeText(ChoseCityActivity.this, "无省份信息",Toast.LENGTH_SHORT).show();
 				}
 				break;
 			case 2:
 				progressDialog.dismiss();
-				citiesAdapter = new ArrayAdapter<City>(ChoseCityActivity.this,
-						android.R.layout.simple_list_item_single_choice, cities);
-				lv_city.setAdapter(citiesAdapter);
-				citiesAdapter.notifyDataSetChanged();
 				if (cities.size() > 0) {
+					citiesAdapter.refresh(cities);
+					citiesAdapter.setSelectItem(0);
+					loadCountry(cities.get(0));
 				} else {
-					Toast.makeText(ChoseCityActivity.this, "无城市信息",
-							Toast.LENGTH_SHORT).show();
+					Toast.makeText(ChoseCityActivity.this, "无城市信息",Toast.LENGTH_SHORT).show();
 				}
 				break;
-
+			case 3:
+				progressDialog.dismiss();
+				if (countries.size() > 0) {
+					countriesAdapter.refresh(countries);
+					countriesAdapter.setSelectItem(0);
+				} else {
+					Toast.makeText(ChoseCityActivity.this, "无县城信息",Toast.LENGTH_SHORT).show();
+				}
+				break;
 			default:
 				break;
 			}
@@ -92,43 +107,54 @@ public class ChoseCityActivity extends BaseActivity {
 	private void initViews() {
 		// TODO Auto-generated method stub
 		dbService = DBTableService.getInstance(this);
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		progressDialog.setMessage("正在初始化数据,请稍后...");
+		progressDialog.setCancelable(false);
+		tv_province = (TextView)this.findViewById(R.id.tv_province);
+		tv_city = (TextView)this.findViewById(R.id.tv_city);
+		tv_country = (TextView)this.findViewById(R.id.tv_country);
 		lv_province = (ListView) this.findViewById(R.id.lv_province);
-		// lv_province.setOnItemClickListener(this);
 		lv_province.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
 				// TODO Auto-generated method stub
 				provincesAdapter.setSelectItem(position);
-				provincesAdapter.notifyDataSetInvalidated();
-				loadCity(provinces.get(0));
+				loadCity(provinces.get(position));
 				LogUtil.i("ChoseCityActivity", "省份:"+ provinces.get(position).getProvinceName());
-//				for(int i=0;i<parent.getCount();i++){
-//		            View v=parent.getChildAt(parent.getCount()-1-i);
-//		            if (position == i) {
-//		                v.setBackgroundColor(Color.RED);
-//		            } else {
-//		                v.setBackgroundColor(Color.TRANSPARENT);
-//		            }
-//		        }
 			}
 		});
 		lv_city = (ListView) this.findViewById(R.id.lv_city);
-
-		progressDialog = new ProgressDialog(this);
-		progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-		progressDialog.setMessage("正在初始化数据,请稍后...");
-		progressDialog.setCancelable(false);
+		lv_city.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View view,
+					int position, long arg3) {
+				// TODO Auto-generated method stub
+				citiesAdapter.setSelectItem(position);
+				loadCountry(cities.get(position));
+				LogUtil.i("ChoseCityActivity", "城市:"+ cities.get(position).getCityName());
+			}
+		});
+		lv_country = (ListView) this.findViewById(R.id.lv_country);
+		lv_country.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View view,
+					int position, long arg3) {
+				// TODO Auto-generated method stub
+				countriesAdapter.setSelectItem(position);
+			}
+		});
 
 		provinces = dbService.getAllProvinces();
-		cities = dbService.getAllCities();
-
+		cities = new ArrayList<City>();
+		countries = new ArrayList<Country>();
 		provincesAdapter = new ProvinceAdapter(this, provinces);
 		lv_province.setAdapter(provincesAdapter);
-
-		citiesAdapter = new ArrayAdapter<City>(this,
-				android.R.layout.simple_list_item_single_choice, cities);
+		citiesAdapter = new CityAdapter(this, cities);
 		lv_city.setAdapter(citiesAdapter);
+		countriesAdapter = new CountryAdapter(this, countries);
+		lv_country.setAdapter(countriesAdapter);
 
 		if (!(provinces.size() > 0)) {
 			loadProvinces();
@@ -146,14 +172,9 @@ public class ChoseCityActivity extends BaseActivity {
 			@Override
 			public void onSuccess(String response) {
 				// TODO Auto-generated method stub
-				boolean flag = Utility.handleProvinceData(dbService, response);
-				provinces = dbService.getAllProvinces();
-				Message message = Message.obtain();
-				message.what = 1;
-				message.obj = flag;
-				handler.sendMessage(message);
+				Utility.handleProvinceData(dbService, response);
+				handler.sendEmptyMessage(1);
 			}
-
 			@Override
 			public void onFailed(String message) {
 				// TODO Auto-generated method stub
@@ -164,33 +185,24 @@ public class ChoseCityActivity extends BaseActivity {
 
 	private void loadCity(final Province province) {
 		// TODO Auto-generated method stub
-		cities = dbService.getAllProvinceCities(Integer.parseInt(province
-				.getProvinceCode()));
+		cities.clear();
+		cities = dbService.getAllProvinceCities(province.getId());
 		if (cities.size() > 0) {
-			citiesAdapter = new ArrayAdapter<City>(ChoseCityActivity.this,
-					android.R.layout.simple_list_item_single_choice, cities);
-			lv_city.setAdapter(citiesAdapter);
-			citiesAdapter.notifyDataSetChanged();
+			citiesAdapter.refresh(cities);
+			citiesAdapter.setSelectItem(0);
+			loadCountry(cities.get(0));
 			return;
 		}
 		progressDialog.setMessage("正在加载城市数据...");
-		HttpUtils.requestHttpGet(progressDialog, Configs.ProvincesUrl
-				+ province.getProvinceCode() + Configs.pxiel,
+		HttpUtils.requestHttpGet(progressDialog, Configs.ProvincesUrl + province.getProvinceCode() + Configs.pxiel,
 				new HttpCallBackListener() {
 					@Override
 					public void onSuccess(String response) {
 						// TODO Auto-generated method stub
-						boolean flag = Utility.handleCityData(dbService,
-								response,
-								Integer.parseInt(province.getProvinceCode()));
-						cities = dbService.getAllProvinceCities(Integer
-								.parseInt(province.getProvinceCode()));
-						Message message = Message.obtain();
-						message.what = 2;
-						message.obj = flag;
-						handler.sendMessage(message);
+						Utility.handleCityData(dbService,response, province.getId());
+						cities = dbService.getAllProvinceCities(province.getId());
+						handler.sendEmptyMessage(2);
 					}
-
 					@Override
 					public void onFailed(String message) {
 						// TODO Auto-generated method stub
@@ -199,25 +211,114 @@ public class ChoseCityActivity extends BaseActivity {
 				});
 	}
 
-	class ProvinceAdapter extends BaseAdapter {
-		private Context context;
-		private List<Province> pList;
-
-		public ProvinceAdapter(Context context, List<Province> pList) {
-			this.context = context;
-			this.pList = pList;
+	private void loadCountry(final City city) {
+		countries.clear();
+		countries = dbService.getAllCityCountries(city.getId());
+		if (countries.size() > 0) {
+			countriesAdapter.refresh(countries);
+			countriesAdapter.setSelectItem(0);
+			return;
 		}
+		progressDialog.setMessage("正在加载县城数据...");
+		HttpUtils.requestHttpGet(progressDialog,Configs.ProvincesUrl + city.getCityCode() + Configs.pxiel,
+				new HttpCallBackListener() {
+					@Override
+					public void onSuccess(String response) {
+						// TODO Auto-generated method stub
+						Utility.handleCountryData(dbService, response , city.getId());
+						countries = dbService.getAllCityCountries(city.getId());
+						handler.sendEmptyMessage(3);
+					}
+					@Override
+					public void onFailed(String message) {
+						// TODO Auto-generated method stub
+						handler.sendEmptyMessage(0);
+					}
+				});
 
+	}
+
+	class ProvinceAdapter extends BaseAdapter {
+		private List<Province> pList;
+		private LayoutInflater mInflater;
+		private int selectItem = -1;
+		public ProvinceAdapter(Context context, List<Province> pList) {
+			this.pList = pList;
+			mInflater = LayoutInflater.from(context);
+		}
+		public void setSelectItem(int selectItem) {
+			this.selectItem = selectItem;
+			tv_province.setText(pList.get(selectItem).getProvinceName());
+			notifyDataSetInvalidated();
+		}
+		public void refresh(List<Province> pList) {
+			this.pList = pList;
+			notifyDataSetChanged();
+		}
 		@Override
 		public int getCount() {
 			// TODO Auto-generated method stub
 			return pList.size();
 		}
-
 		@Override
 		public Object getItem(int position) {
 			// TODO Auto-generated method stub
 			return pList.get(position);
+		}
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			ViewHolder holder = null;
+			if (convertView == null) {
+				holder = new ViewHolder();
+				convertView = mInflater.inflate(R.layout.activity_chose_city_item, null);
+				holder.tv_name = (TextView) convertView.findViewById(R.id.tv_name);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			holder.tv_name.setText(pList.get(position).getProvinceName());
+			if (position == selectItem) {
+				convertView.setBackgroundColor(Color.GREEN);
+			} else {
+				convertView.setBackgroundColor(Color.GRAY);
+			}
+			return convertView;
+		}
+	}
+
+	class CityAdapter extends BaseAdapter {
+		private List<City> cList;
+		private LayoutInflater mInflater;
+		private int selectItem = -1;
+		public CityAdapter(Context context, List<City> cList) {
+			this.cList = cList;
+			mInflater = LayoutInflater.from(context);
+		}
+		public void setSelectItem(int selectItem) {
+			this.selectItem = selectItem;
+			tv_city.setText(cList.get(selectItem).getCityName());
+			notifyDataSetInvalidated();
+		}
+		public void refresh(List<City> cList) {
+			this.cList = cList;
+			notifyDataSetChanged();
+		}
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return cList.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return cList.get(position);
 		}
 
 		@Override
@@ -229,34 +330,84 @@ public class ChoseCityActivity extends BaseActivity {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			// TODO Auto-generated method stub
-			ViewHolder holder = null ;
-			if(convertView == null){
+			ViewHolder holder = null;
+			if (convertView == null) {
 				holder = new ViewHolder();
-				convertView = View.inflate(context, R.layout.activity_chose_city_item, null);
-				holder.tv_name = (TextView)convertView.findViewById(R.id.tv_name);
+				convertView = mInflater.inflate(R.layout.activity_chose_city_item, null);
+				holder.tv_name = (TextView) convertView.findViewById(R.id.tv_name);
 				convertView.setTag(holder);
-			}else{
+			} else {
 				holder = (ViewHolder) convertView.getTag();
 			}
-			holder.tv_name.setText(pList.get(position).getProvinceName());
-			if(position == selectItem){
-				holder.tv_name.setBackgroundColor(color.holo_blue_bright);
-			}else{
-				holder.tv_name.setBackgroundColor(color.holo_green_light);
+			holder.tv_name.setText(cList.get(position).getCityName());
+			if (position == selectItem) {
+				convertView.setBackgroundColor(Color.RED);
+			} else {
+				convertView.setBackgroundColor(Color.WHITE);
 			}
 			return convertView;
 		}
+	}
 
+	class CountryAdapter extends BaseAdapter {
+		private List<Country> cList;
+		private LayoutInflater mInflater;
+		private int selectItem = -1;
+		public CountryAdapter(Context context, List<Country> cList) {
+			this.cList = cList;
+			mInflater = LayoutInflater.from(context);
+		}
 		public void setSelectItem(int selectItem) {
 			this.selectItem = selectItem;
+			tv_country.setText(cList.get(selectItem).getCountryName());
+			notifyDataSetInvalidated();
 		}
-		
-		public void refresh(List<Province> pList){
-			this.pList = pList ;
+
+		public void refresh(List<Country> cList) {
+			this.cList = cList;
 			notifyDataSetChanged();
 		}
-		private int selectItem = -1;
 
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return cList.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return cList.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			// TODO Auto-generated method stub
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			// TODO Auto-generated method stub
+			ViewHolder holder = null;
+			if (convertView == null) {
+				holder = new ViewHolder();
+				convertView = mInflater.inflate(
+						R.layout.activity_chose_city_item, null);
+				holder.tv_name = (TextView) convertView
+						.findViewById(R.id.tv_name);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			holder.tv_name.setText(cList.get(position).getCountryName());
+			if (position == selectItem) {
+				convertView.setBackgroundColor(Color.CYAN);
+			} else {
+				convertView.setBackgroundColor(Color.LTGRAY);
+			}
+			return convertView;
+		}
 	}
 
 	class ViewHolder {
--- src/com/bob/weather/ui/LaunchActivity.java
+++ src/com/bob/weather/ui/LaunchActivity.java
@@ -1,20 +1,7 @@
 package com.bob.weather.ui;
 
-import java.util.List;
-
-import com.bob.weather.config.Configs;
-import com.bob.weather.db.DBTableService;
-import com.bob.weather.inf.HttpCallBackListener;
-import com.bob.weather.model.City;
-import com.bob.weather.model.Province;
-import com.bob.weather.utils.HttpUtils;
-import com.bob.weather.utils.Utility;
-
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -22,7 +9,6 @@ import android.view.animation.Animation.AnimationListener;
 import android.widget.Toast;
 
 public class LaunchActivity extends BaseActivity {
-	private ProgressDialog progressDialog ;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -35,128 +21,6 @@ public class LaunchActivity extends BaseActivity {
 		view = View.inflate(this, R.layout.activity_launch, null);
 		setContentView(view);
 		animationLaunch(view);
-		progressDialog = new ProgressDialog(this);
-		progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-		progressDialog.setMessage("正在初始化数据,请稍后...");
-		progressDialog.setCancelable(false);
-		getProvinceFormNet();
-	}
-
-	private DBTableService dbService = null;
-	private Handler handler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			// TODO Auto-generated method stub
-			super.handleMessage(msg);
-			switch (msg.what) {
-			case 0:
-				Toast.makeText(LaunchActivity.this, "加载数据异常", Toast.LENGTH_SHORT).show();
-				finish();
-				break;
-			case 1:
-				getCitiesFromNet(Integer.parseInt(provinces.get(pCount).getProvinceCode()));
-				pCount ++ ;
-				if(pCount == provinces.size()){
-					cities = dbService.getAllCities();
-					Message message = Message.obtain();
-					message.what = 2 ;
-					handler.sendMessage(message);
-				}
-				break;
-			case 2:
-				getCountiesFromNet(Integer.parseInt(cities.get(cCount).getCityCode()));
-				cCount ++ ;
-				if(cCount == cities.size()){
-					//progressDialog.dismiss();
-					animationLaunch(view);
-				}
-				break;
-
-			default:
-				break;
-			}
-		}
-	};
-	int pCount = 0 ;
-	int cCount = 0 ;
-	List<Province> provinces;
-	List<City> cities;
-	private void getProvinceFormNet() {
-		dbService = DBTableService.getInstance(this);
-		provinces = dbService.getAllProvinces() ;
-		cities = dbService.getAllCities();
-		if (!(provinces.size() > 0)) {
-			//progressDialog.show();
-			HttpUtils.requestHttpGet(null, Configs.ProvincesUrl + Configs.pxiel,
-					new HttpCallBackListener() {
-						@Override
-						public void onSuccess(String response) {
-							// TODO Auto-generated method stub
-							boolean flag = Utility.handleProvinceData(
-									dbService, response);
-							provinces = dbService.getAllProvinces() ;
-							Message message = Message.obtain();
-							message.what = 1 ;
-							message.obj = flag ;
-							handler.sendMessage(message);
-						}
-
-						@Override
-						public void onFailed(String message) {
-							// TODO Auto-generated method stub
-							handler.sendEmptyMessage(0);
-						}
-					});
-		}else{
-			animationLaunch(view);
-		}
-	}
-	
-	private void getCitiesFromNet(final int provinceId){
-		
-		//if(!(cities.size()>0)){
-			HttpUtils.requestHttpGet(null, Configs.ProvincesUrl + provinceId + Configs.pxiel,
-					new HttpCallBackListener() {
-						@Override
-						public void onSuccess(String response) {
-							// TODO Auto-generated method stub
-							boolean flag = Utility.handleCityData(null, response, provinceId);
-							Message message = Message.obtain();
-							message.what = 1 ;
-							message.obj = flag ;
-							handler.sendMessage(message);
-						}
-
-						@Override
-						public void onFailed(String message) {
-							// TODO Auto-generated method stub
-							handler.sendEmptyMessage(0);
-						}
-					});
-		//}
-	}
-	
-	private void getCountiesFromNet(final int cityId){
-		//if(!(cities.size()>0)){
-			HttpUtils.requestHttpGet(null, Configs.ProvincesUrl + cityId + Configs.pxiel,
-					new HttpCallBackListener() {
-						@Override
-						public void onSuccess(String response) {
-							// TODO Auto-generated method stub
-							boolean flag = Utility.handleCountryData(dbService, response, cityId);
-							Message message = Message.obtain();
-							message.what = 2 ;
-							message.obj = flag ;
-							handler.sendMessage(message);
-						}
-
-						@Override
-						public void onFailed(String message) {
-							// TODO Auto-generated method stub
-							handler.sendEmptyMessage(0);
-						}
-					});
-		//}
 	}
 
 	private void animationLaunch(View view) {
@@ -168,6 +32,7 @@ public class LaunchActivity extends BaseActivity {
 			@Override
 			public void onAnimationStart(Animation animation) {
 				// TODO Auto-generated method stub
+				Toast.makeText(LaunchActivity.this, "欢迎使用掌天气", Toast.LENGTH_SHORT).show();
 			}
 
 			@Override
